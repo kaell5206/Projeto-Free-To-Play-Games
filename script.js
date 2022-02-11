1edf39a3c1c28edf615a2b2b353d08b18df762d0
lenonrust <lenon.rust@gmail.com>
2022-02-11 09:26:15 -0300

    Adicionanado funçao para criar Card Games

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 const section = document.querySelectorAll('.pointer');
-const games = document.querySelector('.game-body');
+const gameSection = document.querySelector('.game-body');
+const divSection = document.createElement('div');
+divSection.classList.add('text-light','card-bg', 'card', 'd-flex', 'justify-content-center', 'p-2', 'flex-column', 'card__item');
+const divGameBody = document.createElement('div');
+divGameBody.classList.add('card-body', 'text-dark', 'd-flex', 'gap-3', 'flex-column', 'align-items-center');
+const gameName = document.createElement('h1');
+gameName.classList.add('card-title', 'fs-3');
+const gameDescription = document.createElement('p');
+gameDescription.classList.add('card-text');
+const gameLink = document.createElement('a');
+gameLink.classList.add('btn', 'btn-primary', 'fs-5');
+gameLink.innerHTML = 'Jogue agora!';
+const image = document.createElement('img');
+image.classList.add('card-img-top', 'w-100', 'py-1');
+
 // Autorização da da API via https://rapidapi.com/digiwalls/api/free-to-play-games-database
 
 const getAPI = async (genre) => {
@@ -13,43 +27,44 @@ const getAPI = async (genre) => {
   }
   const response = await fetch(url, options);
   const data = await response.json();
-  console.log(data);
   return data;
 }
 
-const createProductImageElement = (imageSource) => {
-  const img = document.createElement('img');
-  img.className = 'card-img-top w-100 py-1';
-  img.src = imageSource;
-  return img;
-}
+// const createProductImageElement = (imageSource) => {
+//   img.src = imageSource;
+//   return img;
+// }
+
 const createGameCard = ({title, img, description, link}) => {
-  const div = games.appendChild(createCustomElement('div','text-light card-bg card d-flex justify-content-center p-2 flex-column card__item'));
-  div.appendChild(createProductImageElement(img));
-  div.appendChild(createCustomElement('div', 'card-body text-dark d-flex gap-3 flex-column align-items-center'));
-
+  // gameSection.appendChild(divSection);
+  image.src = img;
+  divSection.appendChild(image);
+  divSection.appendChild(divGameBody)
+  gameName.innerText = title;
+  divGameBody.appendChild(gameName);
+  gameDescription.innerText = description;
+  divGameBody.appendChild(gameDescription);
+  gameLink.href = link;
+  divGameBody.appendChild(gameLink);
+  return divSection
 }
 
-const getGenre = () => {
-  section.forEach((div) => {
-    div.addEventListener('click', (event) => {
-    return getAPI(event.target.parentNode.id);
+const getGenre = async () => {
+  await section.forEach((div) => {
+    div.addEventListener('click', async (event) => {
+    const data = await getAPI(event.target.parentNode.id);
+    data.slice(0,10).
+    forEach( (item) => {
+      const obj = {title: item.title, img: item.thumbnail, description: item.short_description, link: item.game_url}
+      console.log(obj);
+      gameSection.appendChild(createGameCard(obj));
+    })    
     })
-    
   })  
 }
 
-// const onScreen = async () => {
-//  const response = await getGenre();
-//  console.log(response)
-//  response.forEach( (item) => {
-//   const obj = {title: item.title, img: item.thumbnail, description: item.short_description, link: item.game_url}
-//   console.log(obj)
-//  })
- 
-// }
 
 window.onload = async () => {
-getGenre();
+await getGenre();
 // onScreen()
 }
